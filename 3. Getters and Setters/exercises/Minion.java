2f67543f006c5272da731c13e50e9a2d5e7ba683
jwhitmore3 <joshuawhitmore3@gmail.com>
2020-11-08 13:39:43 -0600

    completed ch3 Junit test minon

--- 3. Getters and Setters/exercises/Minion.java
+++ 3. Getters and Setters/exercises/Minion.java
@@ -2,4 +2,45 @@ package exercises;
 
 public class Minion {
 
+	private String name;
+	private int eyes;
+	private String color;
+	private String master;
+	
+	//constructor
+	Minion(String name,int eyes,String color,String master)
+	{
+		this.name=name;
+		this.eyes=eyes;
+		this.color=color;
+		this.master=master;
+	}
+	//getters
+	String getName()
+	{
+		return this.name;
+	}
+
+	String getColor() {
+		return this.color;
+	}
+	
+	int getEyes() {
+		return this.eyes;
+		
+	}
+	
+	String getMaster() {
+		return this.master;
+		
+	}
+	
+	void  setMaster(String master) {
+			this.master=master;
+	}
+	
+	
+	
+	
+	
 }
